42ba4296e69454074c1db4869f7dd8fe627e2a7d
LealLee <lil@tradeshift.com>
2019-05-17 13:19:35 +0800

    update package-lock and remove unused files

--- common/model/rule-v4-inspector-hack-url.model.js
+++
@@ -1,7 +0,0 @@
-class RuleV4InspectorHackUrlModel {
-    constructor({name, pattern, enabled = true}){
-        this.name = name;
-        this.pattern = pattern;
-        this.enabled = enabled;
-    }
-}
